e278e8f04c1c44838b4f092fb9acb2064e55d287
unknown <thomasld@stud.ntnu.no>
2021-03-18 05:52:12 +0100

    test

--- src/del2/PiazzaUser.java
+++ src/del2/PiazzaUser.java
@@ -1,5 +1,14 @@
 package del2;
 
 public class PiazzaUser {
+	if String user = "Thomas"{
+			print("kjor da i bakken");
+	};
 
+	private void print(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+			
+	
 }
